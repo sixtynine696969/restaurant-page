fd37bdd84915e51d01349b7d132ae65def3f1e3d
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-08-05 07:53:15 +0200

    Add testing code

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        buttons.forEach(btn => {
+            btn.classList.remove('selected');
+        })
+        button.classList.add('selected');
+    })
+})
